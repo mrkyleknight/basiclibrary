cfed00c7a63796ee2ba1eab08fcc235c3c2dbe43
Kyle Knight <knightkyle@outlook.com>
2023-07-21 01:06:37 -0700

    upload lab 3

--- README.md
+++ README.md
@@ -4,4 +4,17 @@ Name of feature: Arrays, Loops, Imports, ArrayLists
 
 Estimate of time needed to complete: 120min Start time: 1655 Finish time: 2100
 
-Actual time needed to complete: 245 minn
+Actual time needed to complete: 245 min
+
+
+Name of feature: Analyzing Weather Data , Tallying Election
+
+Estimate of time needed to complete: 120min Start time: 1655 Finish time: 2115
+
+Actual time needed to complete: 300 min
+
+Assistance from TA's and Chat GPT 
+
+testAnalyzeWeatherData(), we are checking whether the weather analysis function in the Library class works correctly. 
+
+The tally method counts votes for different candidates and returns the candidate with the most votes. testTally checks if the tally method correctly identifies the candidate who received the highest number of votes from a given set of votes.
--- lib/src/test/java/basiclibrary/LibraryTest.java
+++ lib/src/test/java/basiclibrary/LibraryTest.java
@@ -1,6 +1,9 @@
 package basiclibrary;
 
-//import basiclibrary.Library;
+import org.junit.jupiter.api.Assertions;
+import java.util.ArrayList;
+import java.util.List;
+
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -67,7 +70,49 @@ public class LibraryTest {
         assertArrayEquals(testArrays[2], result);
     }
 
+    @Test
+    public void testAnalyzeWeatherData() {
+        Library library = new Library();
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+
+        String expectedOutput = """
+                High: 72
+                Low: 51
+                Never saw temperature: 63
+                Never saw temperature: 67
+                Never saw temperature: 68
+                Never saw temperature: 69
+                """;
+        String actualOutput = library.analyzeWeatherData(weeklyMonthTemperatures);
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testTally() {
+        Library library = new Library();
+        List<String> votes = new ArrayList<>();
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Shrub");
+        votes.add("Hedge");
+        votes.add("Shrub");
+        votes.add("Bush");
+        votes.add("Hedge");
+        votes.add("Bush");
+
+        String winner = library.tally(votes);
+        Assertions.assertEquals("Bush", winner);
+    }
 }
+
+
 //@Test
 //    @DisplayName("Contains Duplicates")
 //        public void testDuplicates() {
